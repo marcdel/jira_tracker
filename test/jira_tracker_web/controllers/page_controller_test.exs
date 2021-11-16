94ffd5d6fc285791453d100fee8ae872128a899f
Marc Delagrammatikas <delagrammatikas@gmail.com>
2021-11-15 22:47:28 -0800

    update test for bbasic auth

--- test/jira_tracker_web/controllers/page_controller_test.exs
+++ test/jira_tracker_web/controllers/page_controller_test.exs
@@ -2,7 +2,10 @@ defmodule JiraTrackerWeb.PageControllerTest do
   use JiraTrackerWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+    conn = conn
+           |> put_req_header("authorization", "Basic " <> Base.encode64("admin:admin"))
+           |> get("/")
+
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
