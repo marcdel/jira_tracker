9b91a223d2b64bfa4fd91bbbee0269a7bd3cfc04
Marc Delagrammatikas <delagrammatikas@gmail.com>
2021-11-15 22:54:24 -0800

    format

--- test/jira_tracker_web/controllers/page_controller_test.exs
+++ test/jira_tracker_web/controllers/page_controller_test.exs
@@ -2,9 +2,10 @@ defmodule JiraTrackerWeb.PageControllerTest do
   use JiraTrackerWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = conn
-           |> put_req_header("authorization", "Basic " <> Base.encode64("admin:admin"))
-           |> get("/")
+    conn =
+      conn
+      |> put_req_header("authorization", "Basic " <> Base.encode64("admin:admin"))
+      |> get("/")
 
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
