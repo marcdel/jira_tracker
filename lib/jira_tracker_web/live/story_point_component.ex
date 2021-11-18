723477849a64ec7d2bb0512c2236f3c50e191999
Marc Delagrammatikas <delagrammatikas@gmail.com>
2021-11-18 12:50:54 -0800

    add hover styles for stories and make them a bit smaller

--- lib/jira_tracker_web/live/story_point_component.ex
+++ lib/jira_tracker_web/live/story_point_component.ex
@@ -5,22 +5,22 @@ defmodule JiraTrackerWeb.StoryPointComponent do
   def render(%{points: nil, id: id} = assigns) do
     ~H"""
     <div>
-      <span phx-click="point_story" phx-value-id={id} phx-value-points="0" title="Point this story a 0" class="inline-flex items-center cursor-pointer px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      <span phx-click="point_story" phx-value-id={id} phx-value-points="0" title="Point this story a 0" class="inline-flex items-center cursor-pointer px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 hover:bg-gray-200">
         0
       </span>
-      <span phx-click="point_story" phx-value-id={id} phx-value-points="1" title="Point this story a 1" class="inline-flex items-center cursor-pointer px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      <span phx-click="point_story" phx-value-id={id} phx-value-points="1" title="Point this story a 1" class="inline-flex items-center cursor-pointer px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 hover:bg-gray-200">
         1
       </span>
-      <span phx-click="point_story" phx-value-id={id} phx-value-points="2" title="Point this story a 2" class="inline-flex items-center cursor-pointer px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      <span phx-click="point_story" phx-value-id={id} phx-value-points="2" title="Point this story a 2" class="inline-flex items-center cursor-pointer px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 hover:bg-gray-200">
         2
       </span>
-      <span phx-click="point_story" phx-value-id={id} phx-value-points="3" title="Point this story a 3" class="inline-flex items-center cursor-pointer px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      <span phx-click="point_story" phx-value-id={id} phx-value-points="3" title="Point this story a 3" class="inline-flex items-center cursor-pointer px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 hover:bg-gray-200">
         3
       </span>
-      <span phx-click="point_story" phx-value-id={id} phx-value-points="5" title="Point this story a 5" class="inline-flex items-center cursor-pointer px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      <span phx-click="point_story" phx-value-id={id} phx-value-points="5" title="Point this story a 5" class="inline-flex items-center cursor-pointer px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 hover:bg-gray-200">
         5
       </span>
-      <span phx-click="point_story" phx-value-id={id} phx-value-points="8" title="Point this story an 8" class="inline-flex items-center cursor-pointer px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+      <span phx-click="point_story" phx-value-id={id} phx-value-points="8" title="Point this story an 8" class="inline-flex items-center cursor-pointer px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 hover:bg-gray-200">
         8
       </span>
     </div>
@@ -30,7 +30,7 @@ defmodule JiraTrackerWeb.StoryPointComponent do
   @impl true
   def render(%{points: points} = assigns) do
     ~H"""
-    <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+    <span class="inline-flex items-center px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
       <%= points %>
     </span>
     """
