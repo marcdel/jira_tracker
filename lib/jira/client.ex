b7cec60bc511731f12dc8e7e8784acd9f27d0969
Marc Delagrammatikas <delagrammatikas@gmail.com>
2021-11-19 16:29:21 -0800

    add search and custom_fields methods

--- lib/jira/client.ex
+++ lib/jira/client.ex
@@ -10,7 +10,35 @@ defmodule Jira.Client do
   @decorate trace("Jira.Client.backlog", include: [:board_id])
   def backlog(board_id) do
     case AgileAPI.get("/board/#{board_id}/backlog", auth_header()) do
-      {:ok, response} -> {:ok, response |> Map.get(:body) |> Map.get("issues")}
+      {:ok, response} -> {:ok, response |> Map.get(:body) |> Map.get("issues", [])}
+      error -> error
+    end
+  end
+
+  @decorate trace("Jira.Client.search", include: [:jql, :url])
+  def search(jql) do
+    url = "/search?jql=#{URI.encode(jql)}"
+
+    case API.get(url, auth_header()) do
+      {:ok, response} ->
+        issues =
+          response
+          |> Map.get(:body)
+          |> Map.get("issues", [])
+
+        O11y.add_span_attributes(issues_count: Enum.count(issues))
+
+        {:ok, issues}
+
+      error ->
+        error
+    end
+  end
+
+  @decorate trace("Jira.Client.custom_fields")
+  def custom_fields do
+    case API.get("/field", auth_header()) do
+      {:ok, response} -> {:ok, Map.get(response, :body, [])}
       error -> error
     end
   end
