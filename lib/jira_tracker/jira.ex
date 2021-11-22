ed5752b4829f91021fd856ac90df23da016ababb
Marc Delagrammatikas <delagrammatikas@gmail.com>
2021-11-21 22:37:50 -0800

    remove unused functions

--- lib/jira_tracker/jira.ex
+++ lib/jira_tracker/jira.ex
@@ -27,44 +27,4 @@ defmodule JiraTracker.Jira do
         error
     end
   end
-
-  @fetch_backlog &JiraClient.backlog/1
-
-  @decorate trace("JiraTracker.Jira.backlog", include: [:board_id])
-  def backlog(team, fetch_backlog \\ @fetch_backlog) do
-    team.backlog_board_id
-    |> fetch_backlog.()
-    |> case do
-      {:ok, issues_json} ->
-        O11y.add_span_attributes(issue_count: Enum.count(issues_json))
-
-        stories =
-          issues_json
-          |> Enum.reject(&StoryMapper.subtask?/1)
-          |> Enum.map(&StoryMapper.issue_to_story(team, &1))
-
-        O11y.add_span_attributes(stories_count: Enum.count(stories))
-
-        {:ok, stories}
-
-      error ->
-        error
-    end
-  end
-
-  @fetch_other_issues &JiraClient.get_issues/1
-
-  @decorate trace("JiraTracker.Jira.other_stories", include: [:team_name])
-  def other_stories(team, fetch_other_issues \\ @fetch_other_issues) do
-    team.name
-    |> fetch_other_issues.()
-    |> case do
-      {:ok, issues_json} ->
-        O11y.add_span_attributes(issue_count: Enum.count(issues_json))
-        {:ok, Enum.map(issues_json, &StoryMapper.issue_to_story(team, &1))}
-
-      error ->
-        error
-    end
-  end
 end
--- test/jira_tracker/jira_test.exs
+++ test/jira_tracker/jira_test.exs
@@ -27,44 +27,4 @@ defmodule JiraTracker.JiraTest do
       assert [%{id: "1", jira_key: "ISSUE-4321"}] = issues
     end
   end
-
-  describe "backlog" do
-    test "fetches issues from the jira api for the given board" do
-      team = team_fixture(%{name: "My Team", backlog_board_id: 123})
-      fetch_fn = fn 123 -> {:ok, [issue_json(), issue_json()]} end
-
-      {:ok, issues} = Jira.backlog(team, fetch_fn)
-
-      assert Enum.count(issues) == 2
-    end
-
-    test "maps issue json to stories" do
-      team = team_fixture(%{name: "My Team", backlog_board_id: 123})
-      fetch_fn = fn 123 -> {:ok, [issue_json(%{"id" => "1", "key" => "ISSUE-4321"})]} end
-
-      {:ok, issues} = Jira.backlog(team, fetch_fn)
-
-      assert [%{id: "1", jira_key: "ISSUE-4321"}] = issues
-    end
-  end
-
-  describe "other_stories" do
-    test "fetches issues from the jira api for the given board" do
-      team = team_fixture(%{name: "My Team"})
-      fetch_fn = fn "My Team" -> {:ok, [issue_json(), issue_json()]} end
-
-      {:ok, issues} = Jira.other_stories(team, fetch_fn)
-
-      assert Enum.count(issues) == 2
-    end
-
-    test "maps issue json to issue structs" do
-      team = team_fixture(%{name: "My Team"})
-      fetch_fn = fn "My Team" -> {:ok, [issue_json(%{"id" => "1", "key" => "ISSUE-4321"})]} end
-
-      {:ok, issues} = Jira.other_stories(team, fetch_fn)
-
-      assert [%{id: "1", jira_key: "ISSUE-4321"}] = issues
-    end
-  end
 end
